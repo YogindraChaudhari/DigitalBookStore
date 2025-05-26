e39449a148cfe771d984820962216a91ce1afb6b
Yogindra <yogindrachaudhari816@gmail.com>
2025-05-26 21:07:04 +0530

    fixed cors error

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_API_URL || "http://localhost:3000",
+          target: env.VITE_API_URL,
           changeOrigin: true,
           secure: false,
         },
